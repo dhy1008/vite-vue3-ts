a9afaec420adda79d724219763595f4d28d206ec
duhuanyu <huanyu_du@bjdv.com>
2022-08-09 09:33:05 +0800

    脚手架搭建

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
+axios.get("/aa").then(res => {
+	console.log(res);
+});
 </script>
 
 <template>
