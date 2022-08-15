a6d6d9336870937a1e94a7e1c49c11f5b1e77eb9
duhuanyu <huanyu_du@bjdv.com>
2022-08-15 10:23:18 +0800

    搭建基础框架

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,35 @@
 import { createApp } from "vue";
-import "./style.css";
 import App from "./App.vue";
+// reset style sheet
+import "@/styles/reset.scss";
+// CSS common style sheet
+import "@/styles/common.scss";
+// iconfont css
+// import "@/assets/iconfont/iconfont.scss";
+// font css
+// import "@/assets/fonts/font.scss";
+// element plus
+import ElementPlus from "element-plus";
+// element icons
+import * as Icons from "@element-plus/icons-vue";
+// element css
+import "element-plus/dist/index.css";
+// custom element css
+import "@/styles/element.scss";
+// custom directives
+import directives from "@/directives/index";
+// vue Router
+import router from "@/routers/index";
+// pinia store
+import pinia from "@/store/index";
+// svg icons
+// import "virtual:svg-icons-register";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+// 注册element Icons组件
+Object.keys(Icons).forEach(key => {
+	app.component(key, Icons[key as keyof typeof Icons]);
+});
+
+app.use(router).use(pinia).use(directives).use(ElementPlus).mount("#app");
---
+++ src/views/home/index.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="home flx-center">123</div>
+</template>
+
+<script setup lang="ts" name="home">
+import { onMounted } from "vue";
+import { getOutTime } from "@/api/modules/login";
+onMounted(async () => {
+	const res = await getOutTime();
+	console.log(res, "响应结果");
+});
+</script>
+
+<style scoped lang="scss">
+@import "./index.scss";
+</style>
